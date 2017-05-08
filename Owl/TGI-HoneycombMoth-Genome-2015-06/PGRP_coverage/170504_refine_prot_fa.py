a7d0b20619ab7996d02abaad43c8f5a1ad9550f6
hyeol <hyeol911@gmail.com>
2017-05-08 17:04:55 +0900

    gene symbol added to nucleotide accession code

--- Owl/TGI-HoneycombMoth-Genome-2015-06/PGRP_coverage/170504_refine_prot_fa.py
+++ Owl/TGI-HoneycombMoth-Genome-2015-06/PGRP_coverage/170504_refine_prot_fa.py
@@ -1,16 +1,19 @@
 def main():
 	from glob import glob
 	sampleList = glob('*protein.fa')
-	fO = open('PGRP_protein_total2.fa', 'w')
+	fO = open('PGRP_protein_total3.fa', 'w')
 	for x in sampleList:
 		with open(x) as foo:
 			for line in foo:
 				if line[0] == '>':
-					line = line[:-2]+' '+x.split('_')[0]+'\n'
+					line = line.split()
+					line[0] = line[0]+'_'+x.split('_')[0]
+					line = ' '.join(line)
+				line = line.rstrip()+'\n'
 				fO.write(line)
 
 				
-		fO.write('\n')
+#		fO.write('\n')
 	
 	print ''
 
